d37abd956b4628e5dc56d87ba6dfe8c23df36893
Madhuriv2 <lvutukury@gmail.com>
2018-09-24 14:56:24 -0500

    third commit

--- src/main/java/com/yash/SpringBootConfig.java
+++ src/main/java/com/yash/SpringBootConfig.java
@@ -14,6 +14,7 @@ public class SpringBootConfig extends SpringBootServletInitializer {
     }
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootConfig.class, args);
+        
         // it will start application
         
     }
