8641fd4ffff2454d5ec2644d416f84095b613e63
Madhuriv2 <lvutukury@gmail.com>
2018-09-25 09:51:19 -0500

    test

--- src/main/java/com/yash/SpringBootConfig.java
+++ src/main/java/com/yash/SpringBootConfig.java
@@ -14,7 +14,6 @@ public class SpringBootConfig extends SpringBootServletInitializer {
     }
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootConfig.class, args);
-        
         // it will start application
         
     }
